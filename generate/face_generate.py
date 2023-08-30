0a075555aeb4a2756545c8b19be1ed93eae181a9
Tanishq Vashisht <tanishqvashisht12@gmail.com>
2023-08-30 18:48:28 +0530

    hehe

---
+++ generate/face_generate.py
@@ -0,0 +1,158 @@
+import tensorflow as tf
+from flask import Flask, jsonify
+import base64
+from keras.models import load_model
+from keras.utils import array_to_img
+from matplotlib import pyplot as plt
+import os
+from tensorflow import keras
+from keras.models import Sequential
+from keras import layers
+from keras.optimizers import Adam
+from keras.losses import BinaryCrossentropy
+
+app = Flask(__name__)
+
+LATENT_DIM = 100
+WEIGHT_INIT = keras.initializers.RandomNormal(mean=0.0, stddev=0.02)
+CHANNELS = 3
+
+model = Sequential(name='generator')
+
+model.add(layers.Dense(8 * 8 * 512, input_dim=LATENT_DIM))
+model.add(layers.ReLU())
+model.add(layers.Reshape((8, 8, 512)))
+model.add(layers.Conv2DTranspose(256, (4, 4), strides=(2, 2), padding='same', kernel_initializer=WEIGHT_INIT))
+model.add(layers.ReLU())
+model.add(layers.Conv2DTranspose(128, (4, 4), strides=(2, 2), padding='same', kernel_initializer=WEIGHT_INIT))
+model.add(layers.ReLU())
+model.add(layers.Conv2DTranspose(64, (4, 4), strides=(2, 2), padding='same', kernel_initializer=WEIGHT_INIT))
+model.add(layers.ReLU())
+model.add(layers.Conv2D(CHANNELS, (4, 4), padding='same', activation='tanh'))
+generator = model
+# generator.summary()
+
+model = Sequential(name='discriminator')
+input_shape = (64, 64, 3)
+alpha = 0.2
+model.add(layers.Conv2D(64, (4, 4), strides=(2, 2), padding='same', input_shape=input_shape))
+model.add(layers.BatchNormalization())
+model.add(layers.LeakyReLU(alpha=alpha))
+model.add(layers.Conv2D(128, (4, 4), strides=(2, 2), padding='same', input_shape=input_shape))
+model.add(layers.BatchNormalization())
+model.add(layers.LeakyReLU(alpha=alpha))
+model.add(layers.Conv2D(128, (4, 4), strides=(2, 2), padding='same', input_shape=input_shape))
+model.add(layers.BatchNormalization())
+model.add(layers.LeakyReLU(alpha=alpha))
+model.add(layers.Flatten())
+model.add(layers.Dropout(0.3))
+model.add(layers.Dense(1, activation='sigmoid'))
+discriminator = model
+# discriminator.summary()
+
+
+class DCGAN(keras.Model):
+    def __init__(self, generator, discriminator, latent_dim):
+        super().__init__()
+        self.generator = generator
+        self.discriminator = discriminator
+        self.latent_dim = latent_dim
+        self.g_loss_metric = keras.metrics.Mean(name='g_loss')
+        self.d_loss_metric = keras.metrics.Mean(name='d_loss')
+
+    @property
+    def metrics(self):
+        return [self.g_loss_metric, self.d_loss_metric]
+
+    def compile(self, g_optimizer, d_optimizer, loss_fn):
+        super(DCGAN, self).compile()
+        self.g_optimizer = g_optimizer
+        self.d_optimizer = d_optimizer
+        self.loss_fn = loss_fn
+
+    def train_step(self, real_images):
+        batch_size = tf.shape(real_images)[0]
+        random_noise = tf.random.normal(shape=(batch_size, self.latent_dim))
+        with tf.GradientTape() as tape:
+            pred_real = self.discriminator(real_images, training=True)
+            real_labels = tf.ones((batch_size, 1))
+            real_labels += 0.05 * tf.random.uniform(tf.shape(real_labels))
+            d_loss_real = self.loss_fn(real_labels, pred_real)
+            fake_images = self.generator(random_noise)
+            pred_fake = self.discriminator(fake_images, training=True)
+            fake_labels = tf.zeros((batch_size, 1))
+            d_loss_fake = self.loss_fn(fake_labels, pred_fake)
+            d_loss = (d_loss_real + d_loss_fake) / 2
+        gradients = tape.gradient(d_loss, self.discriminator.trainable_variables)
+        self.d_optimizer.apply_gradients(zip(gradients, self.discriminator.trainable_variables))
+        labels = tf.ones((batch_size, 1))
+        with tf.GradientTape() as tape:
+            fake_images = self.generator(random_noise, training=True)
+            pred_fake = self.discriminator(fake_images, training=True)
+            g_loss = self.loss_fn(labels, pred_fake)
+        gradients = tape.gradient(g_loss, self.generator.trainable_variables)
+        self.g_optimizer.apply_gradients(zip(gradients, self.generator.trainable_variables))
+        self.d_loss_metric.update_state(d_loss)
+        self.g_loss_metric.update_state(g_loss)
+
+        return {'d_loss': self.d_loss_metric.result(), 'g_loss': self.g_loss_metric.result()}
+
+
+class DCGANMonitor(keras.callbacks.Callback):
+    def __init__(self, num_imgs=25, latent_dim=100):
+        self.num_imgs = num_imgs
+        self.latent_dim = latent_dim
+        self.noise = tf.random.normal([25, latent_dim])
+
+    def on_epoch_end(self, epoch, logs=None):
+        g_img = self.model.generator(self.noise)
+        g_img = (g_img * 127.5) + 127.5
+        g_img.numpy()
+
+        fig = plt.figure(figsize=(8, 8))
+        for i in range(self.num_imgs):
+            plt.subplot(5, 5, i + 1)
+            img = array_to_img(g_img[i])
+            plt.imshow(img)
+            plt.axis('off')
+        plt.show()
+
+    def on_train_end(self, logs=None):
+        self.model.generator.save('generator.h5')
+
+
+def generate_anime_face():
+    dcgan = DCGAN(generator=generator, discriminator=discriminator, latent_dim=LATENT_DIM)
+    D_LR = 0.0001
+    G_LR = 0.0003
+    dcgan.compile(g_optimizer=Adam(learning_rate=G_LR, beta_1=0.5), d_optimizer=Adam(learning_rate=D_LR, beta_1=0.5),
+                  loss_fn=BinaryCrossentropy())
+    dcgan.generator = load_model('generator.h5')
+    noise = tf.random.normal([1, 100])
+    fig = plt.figure(figsize=(3, 3))
+    g_img = dcgan.generator(noise)
+    g_img = (g_img * 127.5) + 127.5
+    g_img.numpy()
+    img = array_to_img(g_img[0])
+    plt.imshow(img)
+    plt.axis('off')
+    plt.savefig('animeFace.png')
+
+
+@app.route('/generateAnimeFace', methods=['GET'])
+def get_image():
+    try:
+        generate_anime_face()
+        with open("animeFace.png", "rb") as image_file:
+            base64_image = base64.b64encode(image_file.read()).decode('utf-8')
+
+        os.remove("animeFace.png")
+
+        return jsonify({"image_base64": base64_image})
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
